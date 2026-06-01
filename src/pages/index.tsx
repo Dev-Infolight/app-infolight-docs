3cd518f331ec36a5efbf9358ef54f69cd83cb35b
Dev-Infolight <dev@infolight.com.br>
2026-06-01 16:18:46 -0300

    Ajudantando a página inicial da documentação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type {ReactNode} from 'react';
+import {useState, type ReactNode} from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import styles from './index.module.css';
@@ -15,21 +15,24 @@ import Estoque from "@site/static/img/lading-page/estoque.svg";
 import Faturamento from "@site/static/img/lading-page/faturamento.svg";
 import Financeiro from "@site/static/img/lading-page/financeiro.svg";
 import Livros from "@site/static/img/lading-page/livros-fiscais.svg";
-
+import Computer from "@site/static/img/lading-page/computer.svg";
+import Mobile from "@site/static/img/lading-page/mobile.svg";
 
 import { CardInfo } from '../components/CardInfo';
 
+type ViewOptions = 'TempusApp' | 'InfolightApp';
 
 export default function Home(): ReactNode {
 
-  const {siteConfig} = useDocusaurusContext();
+  const [viewOption, setViewOption] = useState<ViewOptions>('InfolightApp');
 
+  const {siteConfig} = useDocusaurusContext();
+  
   return (
     <Layout 
       title={siteConfig.title} 
       description="Documentação oficial infolight."
     >
-    
     <div className={styles.banner}>
         <LogoWhite />
         <p>Um sistema completo para a gestão do seu negócio.</p>
@@ -38,78 +41,96 @@ export default function Home(): ReactNode {
         <h1>Explore a nossa documentação</h1>
     </div>
     <div className={styles.center}>
-       <div className={styles.topics}>
-        <CardInfo 
-          label='Introdução'
-          redirectTo='/docs/introducao'
-          icon={IntroducaoIcon}
-        />
-        <CardInfo 
-          label='Nova venda'
-          redirectTo='/docs/aplicativo/forca-de-vendas/nova-venda'
-          icon={NovaVendaIcon}
-        />
-        <CardInfo 
-          label='Expedição de carga'
-          redirectTo='/docs/aplicativo/expedicao/expedicao-carga'
-          icon={ExpedicaoDeCargaIcon}
-        />
-        <CardInfo 
-          label='Expedição de NFCe'
-          redirectTo='/docs/aplicativo/expedicao/expedicao-NFCE'
-          icon={ExpedicaoNFCeIcon}
-        />
-        <CardInfo 
-          label='Dashboard'
-          redirectTo='/docs/aplicativo/tela-inicial/guia-de-dashboard'
-          icon={DashboardIcon}
-        />
-        <CardInfo 
-          label='Liberação de preços'
-          redirectTo='/docs/aplicativo/gestor/liberacao-de-precos'
-          icon={LiberacaoDePrecosIcon}
-        />
-        
-        
+
+        {
+          (
+            <div className={styles.tabs}>
+              <button className={`${styles.tab} ${viewOption == 'InfolightApp' ? styles.selectedTab : '' }`} onClick={() => setViewOption('InfolightApp')}>
+                <Mobile />
+                <span>Infolight App</span>
+              </button>
+              <button className={`${styles.tab} ${viewOption == 'TempusApp' ? styles.selectedTab : '' }`} onClick={() => setViewOption('TempusApp')}>
+                <Computer />
+                <span>Tempus ERP</span>
+              </button>
+            </div>
+          )
+
+        }
+
+        {
+          viewOption == 'InfolightApp'
+            ? (
+                <div className={styles.topics}>
+                  <CardInfo 
+                    label='Introdução'
+                    redirectTo='/docs/introducao'
+                    icon={IntroducaoIcon}
+                  />
+                  <CardInfo 
+                    label='Nova venda'
+                    redirectTo='/docs/aplicativo/forca-de-vendas/nova-venda'
+                    icon={NovaVendaIcon}
+                  />
+                  <CardInfo 
+                    label='Expedição de carga'
+                    redirectTo='/docs/aplicativo/expedicao/expedicao-carga'
+                    icon={ExpedicaoDeCargaIcon}
+                  />
+                  <CardInfo 
+                    label='Expedição de NFCe'
+                    redirectTo='/docs/aplicativo/expedicao/expedicao-NFCE'
+                    icon={ExpedicaoNFCeIcon}
+                  />
+                  <CardInfo 
+                    label='Dashboard'
+                    redirectTo='/docs/aplicativo/tela-inicial/guia-de-dashboard'
+                    icon={DashboardIcon}
+                  />
+                  <CardInfo 
+                    label='Liberação de preços'
+                    redirectTo='/docs/aplicativo/gestor/liberacao-de-precos'
+                    icon={LiberacaoDePrecosIcon}
+                  />
+                </div>
+              )
+            :
+              (
+                <div className={styles.topics}>
+                  <CardInfo 
+                    label='Adiministrador'
+                    redirectTo='/docs/TempusERP/Administrador/pagina-do-cliente'
+                    icon={Administrador}
+                  />
+                  <CardInfo 
+                    label='Compras'
+                    redirectTo='/docs/TempusERP/Compras/emissao-de-pedidos'
+                    icon={Compras}
+                  />
+                  <CardInfo 
+                    label='Estoque'
+                    redirectTo='/docs/TempusERP/Estoque/ficha-tecnica'
+                    icon={Estoque}
+                  />
+                  <CardInfo 
+                    label='Faturamento'
+                    redirectTo='/docs/TempusERP/Faturamento/cadastro-de-regiao'
+                    icon={Faturamento}
+                  />
+                  <CardInfo 
+                    label='Financeiro'
+                    redirectTo='/docs/TempusERP/Financeiro/prestacao'
+                    icon={Financeiro}
+                  />
+                  <CardInfo 
+                    label='Livros Fiscais'
+                    redirectTo='/docs/TempusERP/Livros-Fiscais/entrada-via-xml'
+                    icon={Livros}
+                  />
+                </div>
+              )
+        }
     </div>
-    </div>
-    <div className={styles.divTitle}>
-        <h1>Documentação do TempusERP</h1>
-        </div>
-      <div className={styles.center}>
-        <div className={styles.topics}>
-          <CardInfo 
-            label='Adiministrador'
-            redirectTo='/docs/TempusERP/Administrador/pagina-do-cliente'
-            icon={Administrador}
-          />
-          <CardInfo 
-            label='Compras'
-            redirectTo='/docs/TempusERP/Compras/emissao-de-pedidos'
-            icon={Compras}
-          />
-          <CardInfo 
-            label='Estoque'
-            redirectTo='/docs/TempusERP/Estoque/ficha-tecnica'
-            icon={Estoque}
-          />
-          <CardInfo 
-            label='Faturamento'
-            redirectTo='/docs/TempusERP/Faturamento/cadastro-de-regiao'
-            icon={Faturamento}
-          />
-          <CardInfo 
-            label='Financeiro'
-            redirectTo='/docs/TempusERP/Financeiro/prestacao'
-            icon={Financeiro}
-          />
-          <CardInfo 
-            label='Livros Fiscais'
-            redirectTo='/docs/TempusERP/Livros-Fiscais/entrada-via-xml'
-            icon={Livros}
-          />
-        </div>
-        </div>
     </Layout>
   );
 }
